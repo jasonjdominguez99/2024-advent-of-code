1482fcf3a3ea28f74cc780aaabce8255edb427a6
Jason Dominguez <jasonjdominguez99@gmail.com>
2024-12-04 00:16:39 +0000

    refactor: make utils in day01 for input processing

--- src/day01/part1.cpp
+++ src/day01/part1.cpp
@@ -1,35 +1,13 @@
 #include "part1.hpp"
+#include "utils.hpp"
 
+#include <cassert>
 #include <cmath>
 #include <iostream>
 
-namespace
-{
-    const size_t InputDataSpacing = 3;
-}
-
 int64_t day01::part1::solve(const std::vector<std::string>& input, const size_t numDigits)
 {
-    std::vector<int> leftList;
-    leftList.reserve(input.size());
-
-    std::vector<int> rightList;
-    rightList.reserve(input.size());
-
-    for (const auto& line : input)
-    {
-        const std::string leftNumberText = line.substr(0, numDigits);
-        const int         leftNumber = std::stoi(leftNumberText);
-        leftList.push_back(leftNumber);
-
-        const size_t      startIndex = numDigits - 1 + InputDataSpacing;
-        const std::string rightNumberText = line.substr(startIndex);
-        const int         rightNumber = std::stoi(rightNumberText);
-        rightList.push_back(rightNumber);
-    }
-
-    std::sort(leftList.begin(), leftList.end());
-    std::sort(rightList.begin(), rightList.end());
+    const auto [leftList, rightList] = Utils::getSortedListsFromInput(input, numDigits);
 
     int64_t sum = 0;
 
