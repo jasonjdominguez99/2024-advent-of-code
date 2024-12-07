5277e7623092da5c5478b5d23aa5cc54cdc742d2
Jason Dominguez <jasonjdominguez99@gmail.com>
2024-12-07 00:47:48 +0000

    refactor: pass `part` to `InputReader` methods as string

--- src/common/inputreader.cpp
+++ src/common/inputreader.cpp
@@ -6,17 +6,17 @@
 
 namespace
 {
-    std::string getInputPath(const std::string& day, bool test, const std::optional<int>& part = std::nullopt)
+    std::string getInputPath(const std::string& day, bool test, const std::optional<std::string>& part = std::nullopt)
     {
         std::string filename = test ? "test_input" : "input";
-        return std::string(INPUT_DIR) + "/" + day + "/" + filename + (part.has_value() ? std::to_string(part.value()) : "") + ".txt";
+        return std::string(INPUT_DIR) + "/" + day + "/" + filename + (part.has_value() ? part.value() : "") + ".txt";
     }
 }
 
-std::vector<std::string> InputReader::readLines(const std::string& day, bool test)
+std::vector<std::string> InputReader::readLines(const std::string& day, bool test, const std::optional<std::string>& part)
 {
     std::vector<std::string> lines;
-    std::ifstream            file(getInputPath(day, test));
+    std::ifstream            file(getInputPath(day, test, part));
     std::string              line;
 
     while (std::getline(file, line))
@@ -27,7 +27,7 @@ std::vector<std::string> InputReader::readLines(const std::string& day, bool tes
     return lines;
 }
 
-std::string InputReader::readFile(const std::string& day, bool test, const std::optional<int>& part)
+std::string InputReader::readFile(const std::string& day, bool test, const std::optional<std::string>& part)
 {
     std::ifstream     file(getInputPath(day, test, part));
     std::stringstream buffer;
--- src/day03/main.cpp
+++ src/day03/main.cpp
@@ -13,7 +13,7 @@ int main()
     std::string part1TestInput = InputReader::readFile("day03", isTest);
     std::cout << "Part 1: " << day03::part1::solve(part1TestInput) << std::endl;
 
-    std::string part2TestInput = InputReader::readFile("day03", isTest, 2);
+    std::string part2TestInput = InputReader::readFile("day03", isTest, "2");
     std::cout << "Part 2: " << day03::part2::solve(part2TestInput) << std::endl;
 
     std::cout << "---------REAL INPUT---------" << std::endl;
